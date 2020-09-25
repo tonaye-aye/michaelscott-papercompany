eeda053f99ce58c377a714a58df864eb0383bc38
Tony Gorgioski <tony.gorgioski@gmail.com>
2020-09-25 20:39:31 +1000

    some changes

--- public/script/app.js
+++ public/script/app.js
@@ -1,9 +1,10 @@
-window.addEventListener("DOMContentLoaded", () => {
+const app = () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
+  window.addEventListener("resize", () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
+};
 
-window.addEventListener("resize", () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
+app();
